57af786d219c2a071690b27f9661e9ca1b1118b2
Johannes <johannes.widkopp@gmail.com>
2024-04-22 14:02:54 +0300

    nyt toimii logini ja rekisteröinti frontista bäkkiin

--- models/authenticationModel.js
+++ models/authenticationModel.js
@@ -2,7 +2,7 @@ const pgPool = require('../dbconnection');
 
 const sql = {
   REGISTER_USER: 'INSERT INTO users("firstName", "lastName", "userName", password, email) VALUES ($1,$2,$3,$4,$5)',
-  GET_PASSWORD: 'SELECT password FROM users WHERE userName=$1'
+  GET_PASSWORD: 'SELECT password FROM users WHERE "userName"=$1'
 }
 
 async function register(firstName, lastName, userName, passwordHash, email) {
@@ -19,7 +19,10 @@ async function getPassword(username) {
   try {
     const result = await pgPool.query(sql.GET_PASSWORD, [username]);
     console.log('Successfully fetched password');
-    return result.rowCount > 0 ? result.rows[0].pw : null;
+    console.log(result)
+    console.log(username)
+    return result.rowCount > 0 ? result.rows[0].password : 'salasana';
+    
   } catch(err) {
     throw new Error('Error getting password', err)
   }
@@ -27,4 +30,4 @@ async function getPassword(username) {
 
 }
 
-module.exports = {register, getPassword}  
+module.exports = {register, getPassword}   

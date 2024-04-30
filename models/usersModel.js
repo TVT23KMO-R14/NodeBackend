6b60cecebf497245a543b5302c63130359eb11fd
Jarno Tauriainen <t2taja00@students.oamk.fi>
2024-04-30 13:32:38 +0300

    Tyhymä virhe oli

--- models/usersModel.js
+++ models/usersModel.js
@@ -3,29 +3,28 @@ const pgPool = require('../dbconnection');
 
 const sql = {
   GET_ALL_USERS: 'SELECT "idUser", "firstName", "lastName", "userName", email FROM users', 
-  GET_USER: 'SELECT "idUser", "firstName", "lastName", "userName" FROM users WHERE "userName"=$1',
+  GET_USER: 'SELECT "idUser", "firstName", "lastName", "userName", email FROM "users" WHERE "idUser"=$1',
 }
 
 
 async function getUsers() {
   try {
     const result = await pgPool.query(sql.GET_ALL_USERS);
-    console.log(result.rows);
+    console.log(result.rows)
     return result.rows;
   } catch (err) {
     throw new Error(err);
   }
 }
 
-async function getUser(username) {
+async function getUser(idUser) {
   try {
-  const result = await pgPool.query(sql.GET_USER, [username]);
-  console.log(result.rows[0]);
-  return result.rows[0];
+    const result = await pgPool.query(sql.GET_USER, [idUser]);
+    console.log(result.rows[0])
+    return result.rows[0];
   } catch (err) {
     throw new Error(err);
   }
-  
 }
 
 module.exports = { getUsers, getUser };

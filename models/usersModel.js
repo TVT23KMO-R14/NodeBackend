b503e58e78126ba4b9f89c5e942b9340f431a642
Jarno Tauriainen <t2taja00@students.oamk.fi>
2024-04-30 12:33:03 +0300

    Userinfon tulostamista ja getgroupsbymemberkorjaus

--- models/usersModel.js
+++ models/usersModel.js
@@ -3,33 +3,30 @@ const pgPool = require('../dbconnection');
 
 const sql = {
   GET_ALL_USERS: 'SELECT "idUser", "firstName", "lastName", "userName", email FROM users', 
-  GET_USER: 'SELECT idUser, firstName, lastName, userName FROM users WHERE userName=$1',
+  GET_USER: 'SELECT "idUser", "firstName", "lastName", "userName" FROM users WHERE "userName"=$1',
 }
 
 
 async function getUsers() {
-  let result = await pgPool.query(sql.GET_ALL_USERS);
-
-  console.log(result.rows);
-  return result.rows;
+  try {
+    const result = await pgPool.query(sql.GET_ALL_USERS);
+    console.log(result.rows);
+    return result.rows;
+  } catch (err) {
+    throw new Error(err);
+  }
 }
 
 async function getUser(username) {
-  let result = await pgPool.query(sql.GET_USER, [username]);
-  return result.rows[0];
-}
-
-/* poistetaan tämä kokonaan ellei tarvitse muokattuna johonkin.
-async function addNote(username, msg) {
   try {
-    await pgPool.query(sql.ADD_NOTE, [msg, username]);
-  } catch(err) {
-      //throw new Error('Username not found!')
-      throw new Error(err)
+  const result = await pgPool.query(sql.GET_USER, [username]);
+  console.log(result.rows[0]);
+  return result.rows[0];
+  } catch (err) {
+    throw new Error(err);
   }
   
 }
-*/
 
 module.exports = { getUsers, getUser };
 
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,31 +1,19 @@
-const { getUsers, getUser } = require('../models/usersModel');
-const { auth } = require('../middleware/auth');
+const { auth } = require('../middleware/auth')
+const { getUser } = require('../models/usersModel')
 
-const router = require('express').Router();
+const router = require('express').Router()
 
-router.get('/all', async (req, res) => {
-    const users = await getUsers();
-    res.json(users);
-});
-
-router.get('/', async (req, res) => {
-    const user = await getUser(req.query.username);
-    console.log(user);
-    res.json(user);
-})
-
-
-/* poistetaan tämä kokonaan jos ei tarvita muokattuna.
-router.post('/note', auth, async (req, res) => {
+router.get('/one', auth, async (req, res) => {
     try {
-        await addNote(res.locals.username, req.body.msg);
-        res.end();
-    } catch(err) {
-        res.status(404).json({error: err.message});
+        console.log("query: ", req.query)
+        console.log("userId: " + req.query.userId)
+        console.log("headers: ", req.headers)
+        const user = await getUser(req.query.userId)
+        res.json(user);
+    } catch (err) {
+        console.log("/one Error: ", err)
+        res.status(404).json({ error: err})
     }
-    
-    res.end();
-});
-*/
+})
 
 module.exports = router;
